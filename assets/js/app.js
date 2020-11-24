ead688c5613a83bfe10adf67077244c48e342dec
Baburineu <58775463+Baburineu@users.noreply.github.com>
2020-11-24 04:39:12 +0200

    Add mobile version and JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,7 @@
+// Add grey color to .nav__links
 const navLinks = document.querySelectorAll('.nav__link');
 
-
 window.addEventListener("scroll", checkScroll );
-
 window.addEventListener('DOMContentLoaded', checkScroll );
 
 function checkScroll() {
@@ -18,3 +17,29 @@ function checkScroll() {
 		}
 	}
 }
+
+// Show/hide menu on click menu button
+const menuBtn = document.querySelector('.menu-btn');
+const menuIco = document.querySelector('.menu-btn__ico');
+const nav = document.querySelector('.nav');
+
+menuBtn.onclick = function () {
+	menuIco.classList.toggle('active');
+	nav.classList.toggle('nav--active');
+};
+
+// Smooth scroll
+$('[data-scroll]').on('click', function (event) {
+	event.preventDefault();
+
+	let elementId = $(this).data('scroll');
+	let elementOffset = $(elementId).offset().top;
+
+	// Hide menu
+	nav.classList.remove('nav--active');
+	menuIco.classList.remove('active');
+
+	$('html, body').animate({
+		scrollTop: elementOffset - 0
+	},1000)
+});
